a568639c5afbc9730d6c1c6702c7588ed329a875
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-02 19:48:59 +0000

    Corrected the URL and added login function

--- services/User.js
+++ services/User.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
+const API_URL = "http://localhost:3333/api/1.0.0"
 
+// returns {id}
 export function addAccount({first_name, last_name, email, password}) {
-  return axios.post("http://localhost:3333/user", {
+  return axios.post(API_URL + "/user", {
     first_name,
     last_name,
+    email,
+    password
+  })
+}
+
+// returns {id, token}
+export function login({email, password}) {
+  return axios.post(API_URL + "/login", {
     email,
     password
   })
