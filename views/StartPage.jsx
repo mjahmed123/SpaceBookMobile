28e39d996dace97e6aa776d4801e598de17d8ced
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-02 19:49:29 +0000

    Partially designed start page

---
+++ views/StartPage.jsx
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+
+
+
+function CustomButton ({title, onPress}) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <AntDesign name="adduser" size={18} color="white" />
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default function StartPage() {
+  return (
+    <View>
+      <Text style={styles.title}>SpaceBook</Text>
+      <CustomButton title="Register" />
+      <CustomButton title="Login" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 30
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: 'center',
+    marginTop: 10,
+    alignSelf: 'center',
+    width: 120,
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderRadius: 20,
+    backgroundColor: '#2570e8'
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    marginLeft: 5
+  }
+})
