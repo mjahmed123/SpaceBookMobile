2c2ea72a4105e6334ae05a824e954bfe5beb54e5
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-03 18:11:18 +0000

    Store async storage token in mobx account store

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { rootStore } from './stores/RootStore';
+import { getToken } from './utils/token';
+import { observer } from 'mobx-react-lite';
+
 
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -8,6 +12,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Register from './views/Register';
 import Login from './views/Login';
 import StartPage from './views/StartPage';
+import Home from './views/Home';
 
 
 const Stack = createNativeStackNavigator();
@@ -20,14 +25,28 @@ const navTheme = {
   },
 };
 
-export default function App() {
+async function getAsyncStorageToken() {
+  const token = await getToken();
+  rootStore.account.setToken(token || undefined, false);
+}
+getAsyncStorageToken();
+
+export default observer(() => {
+  const {account} = rootStore;
+
+  if (account.token === null) {
+    return <NavigationContainer theme={navTheme}></NavigationContainer>
+  }
+
+
   return (
     <NavigationContainer theme={navTheme}>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='StartPage'>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={account.token ? 'Home' : "StartPage"}>
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="StartPage" component={StartPage} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+}) 
