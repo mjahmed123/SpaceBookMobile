bc26454b3f120376772aa482c82ce6fca0dbc5ad
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-04 19:26:31 +0000

    Testing if the feed page loads

---
+++ views/Feed.jsx
@@ -0,0 +1,10 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+
+
+export default function Feed () {
+  return (
+    <View>
+      <Text style={{color: "white"}}>Feed</Text>
+    </View>
+  )
+}
