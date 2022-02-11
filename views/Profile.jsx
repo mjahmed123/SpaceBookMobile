0566c51b295d010267c9b12b839867bb9aeded63
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-11 18:46:29 +0000

    Show add friend button to users who aren't friends

--- views/Profile.jsx
+++ views/Profile.jsx
@@ -5,10 +5,11 @@ import {
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { rootStore } from '../stores/RootStore';
-import { getUserById } from '../services/User';
+import { getUserById, isFriendsWithUser } from '../services/User';
 import Avatar from '../components/Avatar';
 import CustomButton from '../components/CustomButton';
 import color from '../utils/colorSchemes';
+import ProfileFriendsTab from '../components/ProfileFriendsTab';
 
 function Tab({ onPress, isSelected, title }) {
   return (
@@ -24,52 +25,81 @@ Tab.propTypes = {
   title: PropTypes.string,
 };
 
-function Tabs() {
+function Tabs({ friendUserId, navigation }) {
   // 0 is posts, 1 is friends.
   const [selectedTab, setTab] = useState(0);
 
   return (
-    <View style={styles.tabs}>
-      <Tab title="Posts" onPress={() => setTab(0)} isSelected={selectedTab === 0} />
-      <Tab title="Friends" onPress={() => setTab(1)} isSelected={selectedTab === 1} />
+    <View>
+      <View style={styles.tabs}>
+        <Tab title="Posts" onPress={() => setTab(0)} isSelected={selectedTab === 0} />
+        <Tab title="Friends" onPress={() => setTab(1)} isSelected={selectedTab === 1} />
+      </View>
+      {selectedTab === 1 && (
+        <ProfileFriendsTab
+          friendUserId={friendUserId}
+          navigation={navigation}
+        />
+      )}
     </View>
   );
 }
+Tabs.propTypes = {
+  friendUserId: PropTypes.number,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 function FriendIcon() {
   return <Ionicons name="person-add-sharp" size={18} color="white" />;
 }
 
-export default function Profile({ userId }) {
+function AddFriendButton({ isAdded }) {
+  if (!isAdded) return <View />;
+  if (isAdded) return <CustomButton title="Add Friend" Icon={FriendIcon} />;
+}
+AddFriendButton.propTypes = {
+  isAdded: PropTypes.bool,
+};
+export default function Profile({ route, navigation }) {
+  const loggedInUserId = rootStore.account.userId;
+  const userId = route.params?.userId || loggedInUserId;
   const [user, setUser] = useState(null);
+  const [isAdded, setIsAdded] = useState(null);
 
   useEffect(async () => {
-    const id = userId || rootStore.account.userId;
-    const fetchedUser = await getUserById(id);
+    const fetchedUser = await getUserById(userId);
     setUser(fetchedUser);
-  }, []);
+
+    const fetchIsAdded = await isFriendsWithUser();
+    setIsAdded(fetchIsAdded);
+  }, [userId]);
 
   return (
     <View style={styles.container}>
       <Avatar style={styles.avatar} userId={user?.user_id} size={80} />
       <Text style={styles.username}>
-        {user?.first_name}
-        {' '}
-        {user?.last_name}
+        {`${user?.first_name} ${user?.last_name}`}
       </Text>
       <Text style={styles.friendCount}>
-        {user?.friend_count}
-        {' '}
-        friends
+        {`${user?.friend_count} friends`}
       </Text>
-      <CustomButton title="Add Friend" Icon={FriendIcon} />
-      <Tabs />
+      {loggedInUserId !== userId && <AddFriendButton isAdded={isAdded} />}
+      <Tabs key={user?.user_id} friendUserId={user?.user_id} navigation={navigation} />
     </View>
   );
 }
 
 Profile.propTypes = {
-  userId: PropTypes.string,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      userId: PropTypes.number,
+    }),
+  }),
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const styles = StyleSheet.create({
