1461a2c74688129b5ea08428e3d00216c2083e04
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-12 18:37:09 +0000

    Added error handling and made design changes

--- components/ProfileFriendsTab.jsx
+++ components/ProfileFriendsTab.jsx
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import { getFriends } from '../services/User';
 import Friend from './Friend';
 
 export default function ProfileFriendsTab({ friendUserId, navigation }) {
   const [friends, setFriends] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const fetchFriends = () => {
-    getFriends(friendUserId).then(setFriends);
+    getFriends(friendUserId).then(setFriends).catch((error) => {
+      if (error.response.status === 403) {
+        setErrorMessage('You cannot view this users friends as you are not friends with them!');
+        return;
+      }
+      setErrorMessage('Something went wrong. Try again later!');
+    });
   };
   useEffect(() => {
     fetchFriends();
   }, [friendUserId]);
   return (
-    <ScrollView>
-      { !friends?.length
-        && (
-        <Text style={styles.message}>
-          This user doesn&apos;t have any friends.
-        </Text>
-        )}
+    <View style={styles.container}>
+      { errorMessage && (<Text style={styles.message}>{errorMessage}</Text>)}
       {
         friends?.map((user) => (
           <Friend
@@ -30,7 +32,7 @@ export default function ProfileFriendsTab({ friendUserId, navigation }) {
           />
         ))
       }
-    </ScrollView>
+    </View>
   );
 }
 
@@ -42,6 +44,14 @@ ProfileFriendsTab.propTypes = {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(255,255,255,0.1)',
+    padding: 5,
+    borderRadius: 8,
+    margin: 10,
+    marginTop: 10,
+    marginBottom: 5,
+  },
   message: {
     color: 'white',
     opacity: 0.8,
