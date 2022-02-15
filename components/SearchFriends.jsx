91c19c0ec9ad50a810138da6e2ef5da87c4ba8ed
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-15 18:09:26 +0000

    Added load more button and fixed some bugs

--- components/SearchFriends.jsx
+++ components/SearchFriends.jsx
@@ -1,34 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
-  StyleSheet, View,
+  StyleSheet, View, Text,
 } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import { searchUsers } from '../services/User';
 import SearchFriendsTab from './SearchFriendsTab';
 import SearchFriendsBar from './SearchFriendsBar';
 import Friend from './Friend';
+import CustomButton from './CustomButton';
 
-export default function SearchFriends({ setSearchFocused, isSearchFocused }) {
+function LoadMoreIcon() {
+  return <FontAwesome name="refresh" size={24} color="white" />;
+}
+
+export default function SearchFriends({ setSearchFocused, isSearchFocused, navigation }) {
+  const limit = 10;
   const [value, setValue] = useState('');
   const [selectedTab, setSelectedTab] = useState(0);
-  const [currentOffset, setCurrentOffset] = useState(0);
-  const [users, setUsers] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [users, setUsers] = useState(null);
+
+  const [lastFetchedLength, setLastFetchedLength] = useState(0);
+
+  const onLoadMorePressed = async () => {
+    const newOffset = offset + limit;
+    setOffset(newOffset);
+    const fetchedUsers = await searchUsers({
+      query: value,
+      limit,
+      offset: newOffset,
+      searchIn: selectedTab === 0 ? 'all' : 'friends',
+    });
+    setUsers([...users, ...fetchedUsers]);
+    setLastFetchedLength(fetchedUsers.length);
+  };
 
   const onRequestSearch = async (val) => {
-    setCurrentOffset(0);
+    if (!val.trim()) {
+      setLastFetchedLength(0);
+      setUsers(null);
+      return;
+    }
+    setOffset(0);
     const fetchedUsers = await searchUsers({
       query: val,
-      limit: 20,
-      offset: currentOffset,
+      limit,
+      offset,
       searchIn: selectedTab === 0 ? 'all' : 'friends',
     });
     setUsers(fetchedUsers);
-    console.log(fetchedUsers);
+    setLastFetchedLength(fetchedUsers.length);
   };
-  const onTabClicked = (tab) => {
-    setSelectedTab(tab);
-    onRequestSearch();
-  };
+
+  useEffect(() => {
+    onRequestSearch(value);
+  }, [selectedTab]);
 
   return (
     <View>
@@ -40,9 +67,17 @@ export default function SearchFriends({ setSearchFocused, isSearchFocused }) {
       />
       {(!!value || isSearchFocused) && (
         <>
-          <SearchFriendsTab onTabClicked={onTabClicked} selectedTab={selectedTab} />
-          <View style={styles.items}>
-            {users.map((user) => <Friend key={user.user_id} user={user} />)}
+          <SearchFriendsTab onTabClicked={setSelectedTab} selectedTab={selectedTab} />
+          <View>
+            {users?.map((user) => <Friend isSearching onClick={() => navigation.navigate('Profile', { userId: user.user_id })} key={user.user_id} user={user} />)}
+            {lastFetchedLength === limit && <CustomButton style={styles.loadMoreButton} onPress={onLoadMorePressed} title="Load More" Icon={LoadMoreIcon} />}
+
+            {users && !users.length && (
+              <Text style={styles.message}>No results found!</Text>
+            )}
+            {users && users.length && lastFetchedLength !== limit && (
+              <Text style={styles.message}>Reached the end of the results!</Text>
+            )}
           </View>
         </>
       )}
@@ -54,10 +89,19 @@ export default function SearchFriends({ setSearchFocused, isSearchFocused }) {
 SearchFriends.propTypes = {
   setSearchFocused: PropTypes.func,
   isSearchFocused: PropTypes.bool,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 const styles = StyleSheet.create({
-  items: {
-
+  loadMoreButton: {
+    marginBottom: 5,
+  },
+  message: {
+    color: 'white',
+    opacity: 0.8,
+    textAlign: 'center',
+    marginBottom: 5,
   },
 });
