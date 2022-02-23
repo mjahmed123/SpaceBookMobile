902e21e504aaf21abc2823d9a2f383678d04e47d
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-23 17:35:51 +0000

    Added edit and delete post services

--- services/Post.js
+++ services/Post.js
@@ -12,8 +12,43 @@ export function createPost(userId, text) {
   })
     .then((result) => result.data);
 }
+
+export function editPost(userId, postId, text) {
+  return axios.patch(`${API_URL}/user/${userId}/post/${postId}`, { text }, {
+    headers: {
+      'X-Authorization': rootStore.account.token,
+    },
+  })
+    .then((result) => result.data);
+}
+
 export function getPosts(userId) {
   return axios.get(`${API_URL}/user/${userId}/post`, {
+    headers: {
+      'X-Authorization': rootStore.account.token,
+    },
+  })
+    .then((result) => result.data);
+}
+
+export function deletePost(userId, postId) {
+  return axios.delete(`${API_URL}/user/${userId}/post/${postId}`, {
+    headers: {
+      'X-Authorization': rootStore.account.token,
+    },
+  })
+    .then((result) => result.data);
+}
+export function likePost(userId, postId) {
+  return axios.post(`${API_URL}/user/${userId}/post/${postId}/like`, {}, {
+    headers: {
+      'X-Authorization': rootStore.account.token,
+    },
+  })
+    .then((result) => result.data);
+}
+export function unlikePost(userId, postId) {
+  return axios.delete(`${API_URL}/user/${userId}/post/${postId}/like`, {
     headers: {
       'X-Authorization': rootStore.account.token,
     },
