4237e5ae34f986504a80ecfc76ce3dba43443814
mjahmed123 <94318179+mjahmed123@users.noreply.github.com>
2022-02-24 17:40:55 +0000

    Added store for drafts

--- utils/localStore.js
+++ utils/localStore.js
@@ -13,3 +13,22 @@ export const setUserId = async (id) => {
 };
 
 export const getUserId = async () => AsyncStorage.getItem('userId');
+
+// format: {userId, text}[]
+export const getDrafts = async () => {
+  const stringifiedDrafts = await AsyncStorage.getItem('drafts');
+  return JSON.parse(stringifiedDrafts || '[]');
+};
+
+export const addDraft = async (userId, text, firstName) => {
+  const drafts = await getDrafts();
+  drafts.push({
+    userId, text, firstName, timestamp: Date.now(),
+  });
+  await AsyncStorage.setItem('drafts', JSON.stringify(drafts));
+};
+export const removeDraft = async (index) => {
+  const drafts = await getDrafts();
+  const newDrafts = drafts.filter((_, i) => i !== index);
+  await AsyncStorage.setItem('drafts', JSON.stringify(newDrafts));
+};
